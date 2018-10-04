6179bcb0ac6019919d92dd209659ad0486222f30
Caelan <caelan.borowiec@pqi.org>
2018-10-04 11:47:46 -0400

    Added demo file

--- mousemove.js
+++ mousemove.js
@@ -1,13 +1,14 @@
 var cooldown = false;
-$( "body" ).mousemove(function( event ) {
-  if (!cooldown)
-  {
-    alert("You can close things by clicking the X button, d'oh!");
-    cooldown = true;
-    setTimeout(resetCooldown, 15000)
-  }
+$( document ).ready(function() {
+  $( "body" ).mousemove(function( event ) {
+    if (!cooldown)
+    {
+      alert("You can close things by clicking the X button, d'oh!");
+      cooldown = true;
+      setTimeout(resetCooldown, 15000)
+    }
+  });
 });
-
 function resetCooldown() {
     cooldown = false;
 }
